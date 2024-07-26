76d34b9cfc098882160bba1c28acdd3b4a2e28ad
muzammil <muzammil6361@gmail.com>
2024-07-26 14:23:14 +0530

    added jsx page

--- reactmacq/src/Components/McqPage2.jsx
+++ reactmacq/src/Components/McqPage2.jsx
@@ -4,19 +4,38 @@ import  bgimage from '../Assets/bg.jpg'
 import  cap from '../Assets/cap.jpeg'
 
 const McqPage2 = () => {
-    const [radiomcq,radioState]=useState("")
 
+    const [textvalue,textstate]=useState({
+        question:'',
+        option1:"",
+        option2:"",
+        option3:"",
+        option4:"",
+        
+    })
+
+    const handletext=(e)=>{
+        const name=e.target.name;
+        const value=e.target.value;
+
+        textstate((prev)=>{
+            return{...prev,[name]:value}
+        })
+    }
+
+
+    const [radiomcq,radioState]=useState("")
     const HandleRadio=(e)=>{
         radioState(e.target.value)
     }
 
-    const [Rtextv,Rtextstat]=useState('')
-    const handleRadioText=(e)=>{
-        Rtextstat(e.target.value)
-    }
-
+   
     console.log(radiomcq)
-    console.log(Rtextv)
+    console.log(textvalue.option1)
+    console.log(textvalue.option2)
+    console.log(textvalue.option3)
+    console.log(textvalue.option4)
+
     
   return (
     <div className='mcqpage'  style={{backgroundImage:`url(${bgimage})`}}>
@@ -24,23 +43,23 @@ const McqPage2 = () => {
 
         <div className='radioquestion'>
          <div className='quetext'>Enter a Question </div> <br/>
-        <input type="text"  className='mcqtext' placeholder='Enter a question' size="59"/>
+        <input type="text" onChange={handletext} className='mcqtext' placeholder='Enter a question' size="59" name='question'/>
         </div>
 
             <div className='RadioButton'>
-           <input type='radio' name="radiomcq" value={Rtextv} id="mcqA" onClick={HandleRadio}/>
-           <label htmlFor="mcqA"> <input type="text" onChange={handleRadioText} placeholder='Option 1'  size="55" style={{height:"20px"}}/></label> <br/><br/>
+           <input type='radio' name="radiomcq" value={textvalue.option1}   id="mcqA" onClick={HandleRadio}/>
+           <label htmlFor="mcqA"> <input type="text" onChange={handletext} placeholder='Option 1'  size="55" style={{height:"20px"}} name='option1'/></label> <br/><br/>
 
            
 
-           <input type='radio' name="radiomcq" value="Bvalue" id="mcqB" onClick={HandleRadio}/>
-           <label htmlFor="mcqA"> <input type="text" placeholder='Option 2' size="55"/></label><br/><br/>
+           <input type='radio' name="radiomcq" value={textvalue.option2} id="mcqB" onClick={HandleRadio}/>
+           <label htmlFor="mcqA"> <input type="text" onChange={handletext} placeholder='Option 2' size="55" name='option2'/></label><br/><br/>
 
-           <input type='radio' name="radiomcq" value="CValue" id="mcqC" onClick={HandleRadio}/>
-           <label htmlFor="mcqA"> <input type="text" placeholder='Option 3' size="55"/></label><br/><br/>
+           <input type='radio' name="radiomcq" value={textvalue.option3} id="mcqC" onClick={HandleRadio}/>
+           <label htmlFor="mcqA"> <input type="text" onChange={handletext} placeholder='Option 3' size="55" name='option3'/></label><br/><br/>
 
-           <input type='radio' name="radiomcq" value="DValue" id="mcqD" onClick={HandleRadio}/>
-           <label htmlFor="mcqD"> <input type="text" placeholder='Option 4' size="55"/></label>
+           <input type='radio' name="radiomcq" value={textvalue.option4} id="mcqD" onClick={HandleRadio}/>
+           <label htmlFor="mcqD"> <input type="text" onChange={handletext} placeholder='Option 4' size="55" name='option4'/></label>
            </div>
 
            <div className='Radiofbut'>
